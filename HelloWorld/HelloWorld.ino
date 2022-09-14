a2517c20f58dcf4a180248f2dc44b1f898aa393c
JJ226 <janedickerson26@marlborough.org>
2022-09-14 09:11:33 -0700

    Update HelloWorld.ino

--- HelloWorld/HelloWorld.ino
+++ HelloWorld/HelloWorld.ino
@@ -1,3 +1,5 @@
+#include <LiquidCrystal.h>
+
 /*
   LiquidCrystal Library - Hello World
 
@@ -52,14 +54,6 @@ void setup() {
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
   // Print a message to the LCD.
-  lcd.print("hello, world!");
-}
-
-void loop() {
-  // set the cursor to column 0, line 1
-  // (note: line 1 is the second row, since counting begins with 0):
-  lcd.setCursor(0, 1);
-  // print the number of seconds since reset:
-  lcd.print(millis() / 1000);
+  lcd.print("HelloHow are you");
 }
 
